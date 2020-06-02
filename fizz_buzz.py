68c158174f3cfa9c0cfaa6aa54e0dd98db394343
Nyagajrr <muchokijnrbenson@gmail.com>
2020-06-02 18:54:44 +0300

    python script

---
+++ fizz_buzz.py
@@ -0,0 +1,22 @@
+# 1. Fizzbuzz (Choose whatever programming language you want for this)
+# Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+#Done in python
+
+def fizzbuzz(n):
+    list1 = []
+    for i in range(1,n+1):
+        if i % 5 == 0 and i % 3 == 0:
+            list1.append("FizzBuzz")
+        
+        elif i % 5 == 0:
+           list1.append("Buzz")
+        
+        elif i % 3 == 0:
+            list1.append("Fizz")
+        
+        else:
+            list1.append(i)
+    
+    return list1
+
+
