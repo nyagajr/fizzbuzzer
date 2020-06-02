97e709a1254eeece946dd8454636c2897112a5e8
Nyagajrr <muchokijnrbenson@gmail.com>
2020-06-02 19:02:54 +0300

    Correction

---
+++ README.py
@@ -0,0 +1,3 @@
+##How to run it
+1. Clone the script, once it's on the desktop, use terminal to navigate into it.
+3. Type python fizz_buzz.py to show the output
--- fizz_buzz.py
+++ fizz_buzz.py
@@ -4,19 +4,19 @@
 
 def fizzbuzz(n):
     list1 = []
-    for i in range(1,n+1):
+    for i in range(1,101):
         if i % 5 == 0 and i % 3 == 0:
             list1.append("FizzBuzz")
-        
+
         elif i % 5 == 0:
            list1.append("Buzz")
-        
+
         elif i % 3 == 0:
             list1.append("Fizz")
-        
+
         else:
             list1.append(i)
-    
-    return list1
 
+    print(list1)
 
+fizzbuzz([])
